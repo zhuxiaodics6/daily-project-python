980480418be1bffdb383e3543a112255f1d9d1ae
zhuxiaodics6 <2781088493@qq.com>
2021-07-11 11:10:45 +0800

    hhhc

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,5 +1,12 @@
-for a in range(678,689):
-    bai = a//100
+for a in range(1000,10000):
+    qian = a//1000
+    bai = (a//100)%10
     shi = (a//10)%10
     ge = a%10
-    print(bai,shi,ge)
+    qian=qian*qian*qian
+    bai=bai*bai*bai
+    shi=shi*shi*shi
+    ge=ge*ge*ge
+    he=ge+shi+bai+qian
+    if(he==a):
+        print(a)
